0a0f263ef20aea21c9af1c37b1454244a7f607ea
steck4 <59516097+steck4@users.noreply.github.com>
2020-01-04 14:17:16 -0800

    Create RunHmmer.sh

---
+++ Project_Carbonate_v4/final_assemblies/annotation/RunHmmer.sh
@@ -0,0 +1,16 @@
+#PBS -k oe 
+#PBS -m abe
+#PBS -M 
+#PBS -N RunTrinotate
+#PBS -l nodes=1:ppn=2,vmem=100gb,walltime=24:00:00
+
+##Move to correct WD
+cd PWDHERE/final_assemblies/annotation
+
+module load trinotate
+module load hmmer
+
+hmmscan --cpu 16 --domtblout PFAM.out $TRINOTATEDB/Pfam-A.hmm transcripts.reformated.aa > pfam.log &
+signalp -f short -n signalp.out transcripts.reformated.aa & 
+tmhmm --short < transcripts.reformated.aa > tmhmm.out &
+RnammerTranscriptome.pl --transcriptome transcripts.main.fa --path_to_rnammer /N/soft/rhel7/trinotate/rnammer-1.2/rnammer

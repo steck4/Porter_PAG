2397b50210e1eb357709f7f81e869132f52f98da
steck4 <59516097+steck4@users.noreply.github.com>
2020-01-14 13:06:14 -1000

    Update and rename RunBLAST.sh to RunBLASTn.sh

---
+++ Project_Carbonate_v4/final_assemblies/annotation/RunBLASTn.sh
@@ -0,0 +1,12 @@
+#PBS -k oe 
+#PBS -m abe
+#PBS -M 
+#PBS -N RunBLAST
+#PBS -l nodes=1:ppn=2,vmem=100gb,walltime=96:00:00
+
+##Move to correct WD
+cd PWDHERE/final_assemblies/annotation
+
+module load trinotate
+
+blastx -query transcripts.main.fa -db $TRINOTATEDB/uniprot_sprot.pep -num_threads 16 -max_target_seqs 1 -outfmt 6 -evalue 1e-3 > blastx.out

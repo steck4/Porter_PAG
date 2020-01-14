978ca30dc4948b4fa961ab83cc107ddc736fddff
steck4 <59516097+steck4@users.noreply.github.com>
2020-01-14 13:57:39 -1000

    Update RunTrinotate.sh

--- Project_Carbonate_v4/final_assemblies/annotation/RunTrinotate.sh
+++ Project_Carbonate_v4/final_assemblies/annotation/RunTrinotate.sh
@@ -9,11 +9,11 @@ cd PWDHERE/final_assemblies/annotation
 
 module load trinotate
 
-Trinotate Trinotate.sqlite LOAD_tmhmm tmhmm.out 
-Trinotate Trinotate.sqlite LOAD_tmhmm signalp.out 
-Trinotate Trinotate.sqlite LOAD_rnammer transcripts.main.fa.rnammer.gff
-Trinotate Trinotate.sqlite LOAD_pfam PFAM.out
-Trinotate Trinotate.sqlite LOAD_swissprot_blastx blastx.out
-Trinotate Trinotate.sqlite LOAD_swissprot_blastp blastp.out
+Trinotate ./Trinotate.sqlite LOAD_tmhmm tmhmm.out 
+Trinotate ./Trinotate.sqlite LOAD_tmhmm signalp.out 
+Trinotate ./Trinotate.sqlite LOAD_rnammer transcripts.main.fa.rnammer.gff
+Trinotate ./Trinotate.sqlite LOAD_pfam PFAM.out
+Trinotate ./Trinotate.sqlite LOAD_swissprot_blastx blastx.out
+Trinotate ./Trinotate.sqlite LOAD_swissprot_blastp blastp.out
 
-Trinotate Trinotate.sqlite report > Report.xls
+Trinotate ./Trinotate.sqlite report > Report.xls

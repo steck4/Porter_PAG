94625b8bf08f89dadcd3ea629901e5da90e80c83
steck4 <59516097+steck4@users.noreply.github.com>
2020-01-14 20:33:17 -1000

    Create RunKallisto.sh

---
+++ Project_Carbonate_v4/final_assemblies/DE/RunKallisto.sh
@@ -0,0 +1,9 @@
+#load modules
+module load R 
+module load bioconductor 
+module load trinityrnaseq/2.6.6 
+module load kallisto
+
+cd $PDWHERE/
+#make an index of the transcriptome with kallisto
+kallisto index -i combined_kal_index ../okayset/combined.okay.fa

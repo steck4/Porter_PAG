792f027b2ff6cb6de09d903af6ed3aae98c7d2fe
steck4 <59516097+steck4@users.noreply.github.com>
2020-01-15 10:49:09 -1000

    Update RunKallisto.sh

--- Project_Carbonate_v4/final_assemblies/DE/RunKallisto.sh
+++ Project_Carbonate_v4/final_assemblies/DE/RunKallisto.sh
@@ -4,6 +4,13 @@ module load bioconductor
 module load trinityrnaseq/2.6.6 
 module load kallisto
 
-cd $PDWHERE/
+cd $PDWHERE/final_assemblies/DE
+#kallisto manual at - https://pachterlab.github.io/kallisto/manual
+
 #make an index of the transcriptome with kallisto
 kallisto index -i combined_kal_index ../okayset/combined.okay.fa
+
+#Run kallisto quantification for single samples
+kallisto quant -i combined_kal_index -o SAMPLE_kallisto ../../input_files/left.fq ../../input_files/right.fq
+
+

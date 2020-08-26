a94ec031e429e3d60fab6b68322c23b661ff530d
steck4 <59516097+steck4@users.noreply.github.com>
2020-08-26 11:30:32 -1000

    Update RunQC.sh
    Took out busco for short term fix. Current modules need repair by NCGAS

--- Project_Carbonate_v4/final_assemblies/QC/RunQC.sh
+++ Project_Carbonate_v4/final_assemblies/QC/RunQC.sh
@@ -27,8 +27,8 @@ ln -s ../annotation/transcripts.main.fa .
 ln -s ../okayset/combined.okay.fa .
 
 #2) Make script to run BUSCO on all - Needs TESTING
-module load busco/4.0.6
-for f in *fa; do run_BUSCO.py -i $f -o ${f%fa}busco -l /N/soft/rhel7/busco/4.0.6/busco-lineage/eukaryota_odb10 -m tran; done
+#module load busco/4.0.6
+#for f in *fa; do run_BUSCO.py -i $f -o ${f%fa}busco -l /N/soft/rhel7/busco/4.0.6/busco-lineage/eukaryota_odb10 -m tran; done
 
 #3) Make script to run quast on all - DONE
 module unload busco/4.0.6
@@ -37,12 +37,12 @@ for f in *.fa; do quast.py -o ${f%fa}quast $f; done
 
 #4) Make script to combine into a table
 #print names and busco
-echo "Assembly Complete_BUSCOs_(C) Complete_and_single-copy_BUSCOs_(S) Complete_and_duplicated_BUSCOs_(D) Fragmented_BUSCOs_(F) Missing_BUSCOs_(M) Total_BUSCO_groups_searched">BUSCO.table
+#echo "Assembly Complete_BUSCOs_(C) Complete_and_single-copy_BUSCOs_(S) Complete_and_duplicated_BUSCOs_(D) Fragmented_BUSCOs_(F) Missing_BUSCOs_(M) Total_BUSCO_groups_searched">BUSCO.table
 
-for f in *.busco/short_summary_*; do tail -n 6 $f | awk -v f=${f%.busco/short_summary_*} 'BEGIN{ORS=" ";print f}{print $1}END{printf "\n"}' ; done >> BUSCO.table
+#for f in *.busco/short_summary_*; do tail -n 6 $f | awk -v f=${f%.busco/short_summary_*} 'BEGIN{ORS=" ";print f}{print $1}END{printf "\n"}' ; done >> BUSCO.table
 
-sed -i 's/run_//g' BUSCO.table
-sed -i 's/fa-final/fa_final/g' BUSCO.table
+#sed -i 's/run_//g' BUSCO.table
+#sed -i 's/fa-final/fa_final/g' BUSCO.table
 
 #print names and quast
 echo "Assembly #_contigs_(>=_0_bp) #_contigs_(>=_1000_bp) #_contigs_(>=_5000_bp) #_contigs_(>=_10000_bp) #_contigs_(>=_25000_bp) #_contigs_(>=_50000_bp) Total_length_(>=_0_bp) Total_length_(>=_1000_bp) Total_length_(>=_5000_bp) Total_length_(>=_10000_bp) Total_length_(>=_25000_bp) Total_length_(>=_50000_bp) #_contigs Largest_contig Total_length GC_(%) N50 N75 L50 L75 #_N's_per_100_kbp" > quast.table
@@ -50,8 +50,8 @@ echo "Assembly #_contigs_(>=_0_bp) #_contigs_(>=_1000_bp) #_contigs_(>=_5000_bp)
 for f in *.quast/report.txt; do tail -n 22 $f | awk 'ORS=" " {print $NF}END{printf "\n"}'; done >> quast.table
 
 #join
-join -1 1 -2 1 BUSCO.table quast.table | column -t > QC.table
-cat BUSCO.table | column -t > tmp; mv tmp BUSCO.table
-cat quast.table | | column -t > tmp; mv tmp quast.table
+#join -1 1 -2 1 BUSCO.table quast.table | column -t > QC.table
+#cat BUSCO.table | column -t > tmp; mv tmp BUSCO.table
+#cat quast.table | | column -t > tmp; mv tmp quast.table
 
 #use less -S to view the table!

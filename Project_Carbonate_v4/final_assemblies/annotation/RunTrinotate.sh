98ca429eaf43fa6e8f8273b77783baf99d006aa9
steck4 <59516097+steck4@users.noreply.github.com>
2021-04-06 11:38:13 -1000

    Update RunTrinotate.sh

--- Project_Carbonate_v4/final_assemblies/annotation/RunTrinotate.sh
+++ Project_Carbonate_v4/final_assemblies/annotation/RunTrinotate.sh
@@ -1,7 +1,12 @@
-#PBS -k oe 
-#PBS -m abe
-#PBS -M 
-#PBS -N RunTrinotate
+#!/bin/bash
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=user@hawaii.edu
+#SBATCH -J RunAnnotation
+#SBATCH -p general
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --time=10:00:00
+
 #PBS -l nodes=1:ppn=2,vmem=100gb,walltime=10:00:00
 
 ##Move to correct WD
